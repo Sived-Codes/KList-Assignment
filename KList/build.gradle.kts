248f5949f8dc1ca974f8db7a71fb8d5758b76e7b
Prashant Deshmukh <sived.official@gmail.com>
2025-07-25 15:02:52 +0530

    Refactor: Enable Compose and JitPack publishing for KList
    This commit introduces the following changes to the KList module:
    
    - Enabled Jetpack Compose by setting `buildFeatures { compose = true }` and configuring `composeOptions`.
    - Added the `maven-publish` plugin to facilitate publishing to JitPack.
    - Configured publishing for the "release" variant, including sources and Javadoc JARs.
    - Defined a MavenPublication named "release" with group ID "com.github.prashant", artifact ID "klist-dsl", and version "1.0.0".

--- KList/build.gradle.kts
+++ KList/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    id("maven-publish") // For JitPack publishing
 }
 
 android {
@@ -9,7 +10,6 @@ android {
 
     defaultConfig {
         minSdk = 24
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
@@ -23,12 +23,29 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.7.0"
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
     }
 }
 
@@ -46,7 +63,21 @@ dependencies {
 
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            groupId = "com.github.prashant"
+            artifactId = "klist-dsl"
+            version = "1.0.0"
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
 }
